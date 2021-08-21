024e435af89925562e22f641e4ff17a99fbf8c8a
MicheleBrugnara <brugnaramichele@gmail.com>
2021-08-21 10:29:04 +0200

    rnn doesn't work

--- _modules/cleanEEGNet.py
+++ _modules/cleanEEGNet.py
@@ -14,16 +14,20 @@ class cleanEEGNet(LightningModule):
         super().__init__()
         self.f1 = torchmetrics.classification.f_beta.F1()
         self.model = ConvNet()
-        #self.mu = Variable(torch.rand(1,dtype=float).to(p.device), requires_grad=True)
-        self.mu = nn.Parameter(torch.rand(1).to(p.device))
+        self.num_features = 62
+        self.rnn = torch.nn.GRU(62,self.num_features,batch_first = True)
+
+
     def forward(self, x):
-        output = torch.zeros(x.shape[0],x.shape[2]).to(p.device) # (n_batches, n_channels)
-        shadow = torch.zeros(x.shape[2]).to(p.device)
+        conv_output = torch.zeros(x.shape[0],x.shape[1],x.shape[2]).to(p.device) # (n_batches, n_epochs ,n_channels)
+        h = torch.rand(1,x.shape[0],self.num_features).to(p.device)
         for i_b, batch in enumerate(x):
             for i_e, epoch in enumerate(batch):
-                output[i_b,:] = (self.mu * self.model.forward(epoch.view(1,1,epoch.shape[0],epoch.shape[1])) + (1 - self.mu) * shadow)
-                shadow = output[i_b,:].clone()
-        return output
+                conv_output[i_b,i_e,:] = self.model.forward(epoch.view(1,1,epoch.shape[0],epoch.shape[1]))
+
+        output, hn = self.rnn(conv_output,h)
+        #print(hn,hn.shape)
+        return hn[0,:,:]
     
     def loss_fn(self, y_hat, y_target):
         loss = custom_loss()
@@ -41,7 +45,7 @@ class cleanEEGNet(LightningModule):
         label = label[:,:,1]
         output = self(x.float())       
         loss = self.loss_fn(output, label.int())
-        #print("output: ", torch.sigmoid(output), "labels: ", label)
+        print("output: ", torch.sigmoid(output), "labels: ", label)
 
         pred = torch.round(torch.sigmoid(output))
         f1 = self.f1(torch.flatten(pred), torch.flatten(label).int())
